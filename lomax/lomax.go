fd1da0cb40972a895606dabe218d6f1f9548acc1
Simarpreet Singh <simar@opendns.com>
2015-12-25 20:13:45 +0000

    lomax: Refactoring code to live as functions
    Signed-off-by: Simarpreet Singh <simar@opendns.com>

--- lomax/lomax.go
+++ lomax/lomax.go
@@ -9,13 +9,23 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	deptNo   string
-	deptName string
-)
-
 const USER string = "simar"
 const PASSWORD string = "password"
+
+var (
+	deptNo    string
+	deptName  string
+	empNo     int
+	fromDate  string
+	toDate    string
+	birthDate string
+	firstName string
+	lastName  string
+	gender    string
+	hireDate  string
+	salary    int
+	title     string
+)
 
 var operationPtr = flag.String("operator", "", "Operation: SELECT, DELETE, UPDATE, INSERT")
 var countPtr = flag.Int("count", 1, "Repeat: Number of times to repeat the benchmark.")
@@ -33,18 +43,15 @@ func validateInput() {
 	}
 }
 
-func main() {
-	flag.Parse()
-
-	validateInput()
-
+func initializeDB() *sql.DB {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", USER, PASSWORD, *dbPtr))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
-	stmtOut, err := db.Prepare(fmt.Sprintf("%s FROM %s %s", *operationPtr, *tablePtr, *conditionPtr))
+	return db
+}
+func prepareStatement(db *sql.DB, operationPtr string, tablePtr string, conditionPtr string) *sql.Rows {
+	stmtOut, err := db.Prepare(fmt.Sprintf("%s FROM %s %s", operationPtr, tablePtr, conditionPtr))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,8 +59,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
+	defer stmtOut.Close()
+	return rows
+}
 
+func processData(rows *sql.Rows) {
 	for rows.Next() {
 		err := rows.Scan(&deptNo, &deptName)
 		if err != nil {
@@ -61,9 +71,22 @@ func main() {
 		}
 		log.Println(deptNo, deptName)
 	}
-	err = rows.Err()
+	err := rows.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer stmtOut.Close()
+}
+
+func main() {
+	flag.Parse()
+
+	validateInput()
+
+	db := initializeDB()
+	defer db.Close()
+
+	rows := prepareStatement(db, *operationPtr, *tablePtr, *conditionPtr)
+	defer rows.Close()
+
+	processData(rows)
 }
