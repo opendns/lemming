49ab2045808ba013a9016ab6f4ef3c38ff8e8fe3
Levi Junkert <levi@opendns.com>
2016-01-25 20:24:44 +0000

    Add -v flag for killtracer

--- killtracer/main.go
+++ killtracer/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/opendns/lemming/lib/log"
+	"os"
 	"os/signal"
 	"syscall"
 )
+
+const AppVersion = "1.0.0"
 
 func main() {
 	//log.SetDebug(true)
 	signal.Ignore(syscall.SIGWINCH)
 	signal.Ignore(syscall.SIGHUP)
 
+	// Build help menu and print to screen
+	version := flag.Bool("v", false, "print current killtracer version.")
+	flag.Parse()
+	if *version {
+		fmt.Println(AppVersion)
+		os.Exit(0)
+	}
+
+	// Start Application
 	go WatchDebugSettings()
 
 	log.Info("Watching trace pipe for kill signals")
