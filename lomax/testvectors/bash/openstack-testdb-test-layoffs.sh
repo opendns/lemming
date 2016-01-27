3bff797d0406ec589a5b0c26ec7a5c23f2d00910
Simarpreet Singh <simar@opendns.com>
2016-01-27 16:59:14 +0000

    lomax: SQL query parsing code overhaul, adding a RSL
    Signed-off-by: Simarpreet Singh <simar@opendns.com>

--- lomax/testvectors/bash/openstack-testdb-test-layoffs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Summary: This test case simulates SQL queries in a employee layoff scenario
-
-# Query those employees who make more than > 100,000
-./lomax --table="employees a, salaries b" --db="employees" --operation="SELECT" --config=openstack-generic-config.json --cols="a.emp_no, a.birth_date, a.first_name, a.last_name, a.gender, a.hire_date, b.salary, b.from_date, b.to_date" --condition="where b.salary > 100000 limit 10" --user="root" --password="password"
-
-# TODO: Add more queries for this scenario 
---
+++ lomax/testvectors/bash/openstack-testdb-test-paycuts.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Summary: This test case simulates SQL queries in a employee paycut scenario
+
+# Preparation Steps
+go build lomax.go
+
+# Query those employees who make more than > 100,000
+./lomax --db="employees" --operation="SELECT" --table="employees a, salaries b" --condition="WHERE  b.salary > 100000 limit 1" --cols="a.emp_no, a.birth_date, a.first_name, a.last_name, a.gender, a.hire_date, b.salary, b.from_date, b.to_date" --user="root" --password="password" --config=openstack-generic-config.json
+
+# Insert a Random employee
+./lomax --db="employees" --operation="INSERT" --flag="IGNORE" --table="employees" --cols="emp_no, birth_date, first_name, last_name, gender, hire_date" --condition="1010101, 1980-01-01, 'John', 'Doe', 'M', 2016-01-01" --config=openstack-generic-config.json --user="root" --password="password" --count=1
+
+# Update the employees with > 100,000 salary from employees table 
+./lomax --db="employees" --operation="UPDATE" --table="salaries" --condition="salary=90000 WHERE salary > 100000 limit 1" --cols=" " --config=openstack-generic-config.json --user="root" --password="password" --count=1
+
+# Delete the employees with 100,000 salary from dept_emp table
+./lomax --db="employees" --operation="DELETE" --table="dept_emp" --condition="emp_no IN ( SELECT emp_no FROM ( SELECT salary FROM salaries WHERE salary > 100000) as p) limit 1" --config=openstack-generic-config.json --user="root" --password="password"
+
+# Delete the employees with 100,000 salary from dept_manager table
+./lomax --db="employees" --operation="DELETE" --table="dept_manager" --condition="emp_no IN ( SELECT emp_no FROM ( SELECT salary FROM salaries WHERE salary > 100000) as p) limit 1" --config=openstack-generic-config.json --user="root" --password="password"
+
+# Delete the employees with 100,000 salary from titles table
+./lomax --db="employees" --operation="DELETE" --table="titles" --condition="emp_no IN ( SELECT emp_no FROM ( SELECT salary FROM salaries WHERE salary > 100000) as p) limit 1" --config=openstack-generic-config.json --user="root" --password="password"
+
+# Delete the employees with 100,000 salary from salary table
+./lomax --db="employees" --operation="DELETE" --table="salaries" --condition="salary > 100000 limit 1" --config=openstack-generic-config.json --user="root" --password="password"
