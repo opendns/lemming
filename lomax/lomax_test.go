7c8b8769bb4f1872ace6e62ae56ce1f982ea0e4d
Simarpreet Singh <simar@opendns.com>
2016-02-09 20:21:30 +0000

    lomax: Adding the ability to run on a remote host
    Signed-off-by: Simarpreet Singh <simar@opendns.com>

--- lomax/lomax_test.go
+++ lomax/lomax_test.go
@@ -10,6 +10,8 @@ func TestTravis(t *testing.T) {
 	configParse("openstack-generic-config.json", "openstack-generic-tests.json")
 	cases := []struct {
 		testTableInput     string
+		testHostNameInput  string
+		testPortInput      string
 		testDBInput        string
 		testOperationInput string
 		testFlagInput      string
@@ -18,10 +20,10 @@ func TestTravis(t *testing.T) {
 		testConditionInput string
 		outputWanted       bool
 	}{
-		{"employees", "employees", "SELECT", " ", "false", "*", "LIMIT 2", true},
+		{"employees", "localhost", "3306", "employees", "SELECT", " ", "false", "*", "LIMIT 2", true},
 	}
 	for _, c := range cases {
-		db := initializeDB(USER, PASSWORD, c.testDBInput)
+		db := initializeDB(USER, PASSWORD, c.testHostNameInput, c.testPortInput, c.testDBInput)
 		returnedRows := prepareStatement(db, c.testOperationInput, c.testFlagInput, c.testRandomInput, c.testColumnsInput, c.testTableInput, c.testConditionInput)
 		returnedOutput := processData(returnedRows, c.testColumnsInput, c.testTableInput)
 		if returnedOutput != c.outputWanted {
